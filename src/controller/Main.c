343e8e9138f19b694fe8cd1848bc396c11542534
Mickwerty <chistianmickaelr@gmail.com>
2024-06-16 21:44:31 +0200

    Ajout Get opt

--- src/controller/Main.c
+++ src/controller/Main.c
@@ -1,16 +1,44 @@
 #include "../../include/controller/Controller.h"
+#include <getopt.h>
 
 int main(int argc, char *argv[]) {
     // Vérification des arguments
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <image_file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s [--circles] <image_file>\n", argv[0]);
         return 1;
     }
-    int useCircles = argc >= 3 && strcmp(argv[2], "--circles") == 0;
+
+    static struct option long_options[] = {
+        {"circles", no_argument, NULL, 'c'},
+        {NULL, 0, NULL, 0}
+    };
+
+    int useCircles = 0;
+    int option_index = 0;
+    int opt;
+
+    while ((opt = getopt_long(argc, argv, "c", long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 'c':
+                useCircles = 1;
+                break;
+            case '?':
+                // Affiche un message d'erreur pour les options non reconnues
+                fprintf(stderr, "Option inconnue\n");
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [--circles] <image_file>\n", argv[0]);
+        return 1;
+    }
+
+    const char *image_file = argv[optind];
 
     // Initialisation de la fenêtre
     initialize_window();
-    MLV_Image *image = load_image(argv[1]);
+    MLV_Image *image = load_image(image_file);
     quadnode *tree = NULL;
 
     // Boucle principale
